a2c60c8894f11e0e000eec275e11e3124ccb6b29
Avinash Jaiswal <Avinash.Jaiswal@users.noreply.github.com>
2023-09-19 23:40:53 +0530

    installed nz-zorro

--- README.md
+++ README.md
@@ -27,4 +27,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 
 ## Add material in angular
-npm add @angular/material
+`npm add @angular/material`
+
+## Add ng-zorro from ant
+`ng add ng-zorro-antd`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MainLayoutComponent } from './modules/layout/components/main-layout/main-layout.component';
 import { MainWhiteLayoutComponent } from './modules/layouts/white-theme/components/main-white-layout/main-white-layout.component';
 import { NotFoundComponent } from './modules/not-found/components/not-found/not-found.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+registerLocaleData(en);
 
 @NgModule({
     declarations: [
@@ -30,8 +38,12 @@ import { NotFoundComponent } from './modules/not-found/components/not-found/not-
         MatIconModule,
         MatSidenavModule,
         MatListModule,
+        FormsModule,
+        HttpClientModule,
     ],
-    providers: [],
+    providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
